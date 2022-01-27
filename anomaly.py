7c3f6b8092e8694cbb002457a2c8789010065501
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2022-01-27 16:07:46 +0530

    cleaning

--- anomaly.py
+++ anomaly.py
@@ -23,3 +23,33 @@ VALIDATE_SIZE = 0.2
 # setting random seeds for libraries to ensure reproducibility
 np.random.seed(RANDOM_SEED)
 rn.seed(RANDOM_SEED)
+
+df.columns = map(str.lower, df.columns)
+df.rename(columns={'class': 'label'}, inplace=True)
+
+# print first 5 rows to get an initial impression of the data we're dealing with
+df.head()
+
+# manual parameter 
+RATIO_TO_FRAUD = 15
+
+# dropping redundant columns
+df = df.drop(['time', 'amount'], axis=1)
+
+# splitting by class
+fraud = df[df.label == 1]
+clean = df[df.label == 0]
+
+# undersample clean transactions
+clean_undersampled = clean.sample(
+    int(len(fraud) * RATIO_TO_FRAUD),
+    random_state=RANDOM_SEED
+)
+
+# concatenate with fraud transactions into a single dataframe
+visualisation_initial = pd.concat([fraud, clean_undersampled])
+column_names = list(visualisation_initial.drop('label', axis=1).columns)
+
+# isolate features from labels 
+features, labels = visualisation_initial.drop('label', axis=1).values, \
+                   visualisation_initial.label.values
